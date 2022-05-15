ef95c7e28371b93fbb23e886e613e55e106747b5
Dawson Graf <38477934+Giraffe2012@users.noreply.github.com>
2022-05-15 14:14:55 -0700

    Add files via upload

--- testDriver3.s
+++ testDriver3.s
@@ -7,6 +7,7 @@
 .data
 szTemp: .skip	21
 szTemp1: .skip	21
+szTemp2: .skip	21
 str1:  .asciz "The Cat in the Hat"
 str2:  .asciz "\n"
 str3:  .asciz  "By Dr. Seuss"
@@ -18,12 +19,14 @@ str8:	.asciz "\n\n\n"
 str9:	.asciz "Enter Index to Delete: "
 str10:	.asciz "Enter Index to Edit: "
 str11:	.asciz "Enter String: "
+str12:	.asciz "Matching Strings: \n"
 szWarn:	.asciz "Node does not exist.\n\n"
 szNull: .asciz "\n\n"
 cLF: .byte 10 //char value of new line
+ptrString:  .quad 0
 data:	.quad	0
 headPtr:  .quad  0
-tailPtr:  .quad  
+tailPtr:  .quad  0
 
 
 	.global _start		//Declare start
@@ -179,6 +182,8 @@ _start:					//Program Start
 	BL delete
 	
 	bl traverse			//traverse
+	
+	Bl search
 	
 	
 exit:
@@ -385,9 +390,134 @@ addNode:
 	RET
 //////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////
+search:
+	STR X30, [SP, #-16]!
+	
+	LDR X0, =cLF
+	BL putch
+	
+	LDR X0, =cLF
+	BL putch
+	
+	LDR X0, =str11
+	BL putstring
+
+	LDR X0,=szTemp		//Specify where to store
+	MOV X1,#21			//Specify string length
+	BL getstring		//Call getstring
+	
+	LDR X0, =szNull
+	BL putstring
+	
+	LDR X0, =str12
+	BL putstring
+	
+	break13:
+	
+	LDR X0,=szTemp
+	BL Str_toUpperCase		//Call toUpper
+	
+	LDR	X19, =headPtr
+	LDR	X19, [X19]
+	
+	
+	
+	searchTop:
+	CMP X19,#0
+	BEQ searchBot
+	LDR	X0, [X19]
+	MOV X3, X0
+	MOV X15, X0
+	BL  stringLength
+	MOV X16, X2
+	MOV X5, #0
+	
+	LDR X1, =szTemp2
+	STR X5, [X1]
+	STR X5, [X1, #8]
+	
+	LDR X1, =szTemp2
+	BL str_Copy
+	
+	LDR X0, =szTemp2		//Load ptrString into X0
+	bl Str_toUpperCase		//Call toUpper
+	
+	break14:
+	
+	LDR X1, =szTemp
+	LDR X3, =szTemp2
+	
+	BL compare
+	
+	
+	ADD	X19,X19,#8
+	LDR X19,[X19]
+	B	searchTop
+	searchBot:
 
 
+	LDR  X30, [SP], #16
+	RET
+//////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////
+	compare:
+	STR X30, [SP, #-16]!
+	STR X3, [SP, #-16]!
+	STR X1, [SP, #-16]!
+	
+	LDR X0,=szTemp
+	BL stringLength
+	
+	LDR  X1, [SP], #16
+	LDR  X3, [SP], #16
+	
+	break15:
+	
+	MOV X11, #0
+	MOV X10, #0
+	
+	compareTop:				//Top of the first function
+	CMP X2, #0			//Compare X2 to 0
+	BEQ comparebot1			//if X2 = 0 branch
+	LDRB W4, [X3, X11]	//Load the first byte
+	LDRB W5, [X1, X10]	//Store the first byte
+	
+	CMP X4, X5
+	BNE compareFail
+	
+	ADD X10, X10, #1
+	ADD X11, X11, #1
+	SUB X2, X2, #1		//Decrement X2
+	MOV X6, #1
+	
+	b compareTop				//Branch to top
+	
+	comparebot1:			//Bottom of first function
+	
+	MOV X0, X15
+	BL putstring
+	
+	LDR X0, =cLF
+	BL putch
+	
+	B compareEnd
+	
+	compareFail:
+	CMP X6, #1
+	BEQ compareEnd
+	
+	CMP X11, X16
+	BEQ compareEnd
+	ADD X11, X11, #1
+	
+	B compareTop
+	
+	compareEnd:
+	LDR  X30, [SP], #16
+	RET
+//////////////////////////////////////////////////////////////////////
 
 
 
